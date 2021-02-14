c312a99867632c211fc9d3025723d2748d53bd74
Ramzi Bouchair <bouchairramzi@gmail.com>
2021-02-14 05:13:37 +0100

    sending a message and showin it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React,{useState} from 'react'; 
 import './App.css';
 import firebase from 'firebase/app';
 import 'firebase/firestore'; 
@@ -22,9 +22,10 @@ firebase.initializeApp({
 const auth = firebase.auth();
 const firestore = firebase.firestore();  
 
-const [user] = useAuthState(auth);
+
 
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
       <header className="App-header">
@@ -41,12 +42,63 @@ function SignIn(){
       const provider = new firebase.auth.GoogleAuthProvider(); 
       auth.signInWithPopup(provider); 
   } 
+
+function SignOut(){
+  return auth.currentUser && (
+    <button onClick={()=> auth.signOut()}>Sign Out</button>
+  )
+}
   
   
   return (<button onClick={SignInWithGoogle}> Sign In with Google</button>) 
 }
 
 function ChatRoom(){
+  // au Chat room nous avons des messages : 
+  const messagesRef = firestore.collection('messages'); // reference vers la collection message de firestore 
+  const query = messagesRef.orderBy('createdAt').limit(25);  // requeter des éléments dans une base de données 
+
+  const [messages] = useCollectionData(query, {idField: 'id'}); 
+
+  const [formValue, setFormValue] = useState(''); 
+
+  const sendMessage = async(e) => {
+    e.preventDefault();
+    const {uid , photoUrl} = auth.currentUser;
+    await messagesRef.add({
+      text:formValue, 
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(), 
+      uid, 
+      photoUrl
+    });
+    setFormValue(''); // initialiser le champs d'envoie
+  }
+  
+  return (
+      <> 
+        <div>
+          {messages && messages.map (msg=> <ChatMessage key={msg.id} message={msg} />)}
+        </div>
+
+        <form onSubmit={sendMessage}>
+          <input value={formValue} onChange={(e)=> setFormValue(e.target.value)}></input>
+
+          <button type="submit">🔥</button>
+        </form>
+      </>
+  )
+}
+
+function ChatMessage (prop){
+  const {text, uid, photoUrl}= prop.message;
+
+  const classMessage = uid === auth.currentUser.uid ? 'sent' : 'received';  
+  return (
+    <div class={'message ${classMessage}'}>
+        <img src={photoUrl} />
+        <p>{text}</p>
+    </div>
+  )
 
 }
 
